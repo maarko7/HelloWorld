0f2ffc94350f34167cfdf0816da7bb18714a4608
Marko Santor <markosantor99@gmail.com>
2023-03-09 13:47:35 +0100

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+import animals.Animal;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+	
+	System.out.println("Now accessing HelloWorld");
+
+	Animal animal = new Animal();
+
+	}
+}
---
+++ animals/Animal.java
@@ -0,0 +1,11 @@
+package animals;
+
+public class Animal {
+
+	public Animal(){
+
+	System.out.println("Now accessing Animals");
+	
+	}
+
+}
